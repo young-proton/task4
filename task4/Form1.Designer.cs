b5881c9a30b4e523aecf15dfb5527991c2a01770
Matvii Burchak <burchak@ut.ee>
2024-10-17 22:39:29 +0300

    Добавьте файлы проекта.

---
+++ task4/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace task4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputbox = new System.Windows.Forms.TextBox();
+            this.answer = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputbox
+            // 
+            this.inputbox.Location = new System.Drawing.Point(279, 23);
+            this.inputbox.Name = "inputbox";
+            this.inputbox.Size = new System.Drawing.Size(100, 29);
+            this.inputbox.TabIndex = 1;
+            // 
+            // answer
+            // 
+            this.answer.AutoSize = true;
+            this.answer.Location = new System.Drawing.Point(93, 97);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(0, 24);
+            this.answer.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 79);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Count";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Answer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input number to take factorial";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(514, 242);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.inputbox);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Fsctorial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox inputbox;
+        private System.Windows.Forms.Label answer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ task4/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Int64 factor_answer = 1;
+                if (!Int64.TryParse(inputbox.Text, out Int64 number) || number < 0 || number>24)
+                {
+                    answer.Text = "Please enter a valid non-negative integer or < 24.";
+                    return;
+                }
+
+                do
+                {
+                    factor_answer *= number;
+                    number--;
+                }
+                while (number > 0);
+
+                answer.Text = $"{factor_answer}";
+
+               
+            }
+            catch 
+            {
+                answer.Text = $"Error";            }
+
+            
+         
+                
+        }
+    }
+}
